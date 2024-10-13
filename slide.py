6d8c65c123b16f9a955a90c77340186552d83bf7
bichdao2005 <bichdao0118@gmail.com>
2024-10-13 17:24:58 +0700

    Add files via upload

---
+++ slide.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 13 14:50:01 2024
+
+@author: Student
+"""
+#SLIDE 30
+dictUni = {
+    5201889: "MIT",
+    "IG" :"Tiền Giang",
+    "LA":"Long An",
+    "ĐN":"Đồng Nai",
+    "BT":"Bình Thuận",
+    "VL": "Vĩnh Long",
+ }                                                                                                                
+#Slide 33
+dictUni ={
+   520189:"MIT",
+   "IT":"Information Technology",
+   "SE":"Computer Engineering",
+   "CS":"Computer Science",
+   }
+print(dictUni[520189])
+print(dictUni["SE"])
+
+#slide 34
+dictUni ={
+   520189:"MIT",
+   "IT":"Information Technology",
+   "SE":"Computer Engineering",
+   "CS":"Computer Science",
+   }
+dictUni["DS"]="Data Science"
+print(dictUni)
+
+#
